17788091eaba7e3bf04f334ca1dec21dcb37c920
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-09-13 12:22:20 -0400

    initial commit

--- src/data/materialsData.js
+++ src/data/materialsData.js
@@ -4,7 +4,7 @@ export const getMaterials = () => {
   return fetch(_apiUrl).then((r) => r.json());
 };
 
-//export a function here that gets a ticket by id
+//export a function here that gets a material by id
 export const getMaterial = (id) => {
   return fetch(`${_apiUrl}/${id}`).then((r) => r.json());
 };
