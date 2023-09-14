eabb99cc361aceb9452dcf006280c9e7da9b29c7
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-09-14 14:10:31 -0400

    return item completed

--- src/data/checkoutsData.js
+++ src/data/checkoutsData.js
@@ -3,3 +3,12 @@ const _apiUrl = "/api/checkouts";
 export const getCheckouts = () => {
     return fetch(_apiUrl).then((r) => r.json());
 };
+
+export const returnCheckouts = (id) => {
+    return fetch(`${_apiUrl}/return/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+};
